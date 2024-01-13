6c90b3ea834229194f580390d50351b43baa5bd3
Nikolai2038 <nikolai2038@yandex.ru>
2024-01-13 14:38:20 +0300

    Add some settings from Debian's default .bashrc

--- main.sh
+++ main.sh
@@ -36,6 +36,25 @@ C_SUCCESS="\[\033[38;5;02m\]"
 accuracy=2
 # ----------------------------------------
 
+# ----------------------------------------
+# From Debian .bashrc
+# ----------------------------------------
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+# ----------------------------------------
+
 # Не отображаем информацию о выполнении прошлой команды для самого первого вывода в сессии
 is_first_command=-1
 alias clear="is_first_command=-1; clear"
