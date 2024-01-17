b09fd1a4eff606c093f2e4f2910edee7d1c9fe75
Nikolai2038 <nikolai2038@yandex.ru>
2024-01-17 20:39:23 +0300

    Revert "Attempts to create startup script"
    This reverts commit a66e375522a6ff183d80ec9ef6c3b8e24c92ee2a.

--- startup_as_root.d/n2038-network-fix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# "/etc/resolv.conf" link remains after working in WSL, so we need to remove it in Debian
-if [ -h "/etc/resolv.conf" ] && [ ! -e "/etc/resolv.conf" ]; then
-  rm "/etc/resolv.conf"
-fi
