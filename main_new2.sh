013706906012347681e4f2ee54929197cf65ea36
Nikolai2038 <nikolai2038@yandex.ru>
2024-03-11 21:11:53 +0300

    Cleaning code

--- main_new2.sh
+++ main_new2.sh
@@ -3,26 +3,16 @@
 # Code to execute when starting "sh"
 export eval_code_for_sh=""
 
-sed_escape() {
-  echo "$@" | sed -e 's/[]\/$*.^;|{}()[]/\\&/g' || return "$?"
-  return 0
-}
-
 # Creates variable, which contains full function declaration and body.
 # This variable will be exported and be available in "sh" ("sh" can't export functions).
 export_function_for_sh() {
   function_name="${1}" && { shift || true; }
-  function_name_as="${1:-"${function_name}"}" && { shift || true; }
-  is_escape_colors="${1:-"${function_name}"}" && { shift || true; }
-  eval "export ${function_name_as}_EXPORT"
-  eval "${function_name_as}_EXPORT=\"\$(typeset -f \"${function_name}\")\"" || return "$?"
-  if [ "${function_name_as}" != "${function_name}" ]; then
-    eval "${function_name_as}_EXPORT=\"\$(echo \"\${${function_name_as}_EXPORT}\" | sed 's/$(sed_escape "${function_name}")/$(sed_escape "${function_name_as}")/')\"" || return "$?"
-  fi
+  eval "export ${function_name}_EXPORT"
+  eval "${function_name}_EXPORT=\"\$(typeset -f \"${function_name}\")\"" || return "$?"
   eval_code_for_sh="${eval_code_for_sh}
     $(eval "
-      if [ -n \"\${${function_name_as}_EXPORT}\" ]; then
-        echo \"\${${function_name_as}_EXPORT}\";
+      if [ -n \"\${${function_name}_EXPORT}\" ]; then
+        echo \"\${${function_name}_EXPORT}\";
       fi;
     ")
   "
@@ -30,6 +20,11 @@ export_function_for_sh() {
   return 0
 }
 export_function_for_sh export_function_for_sh
+
+sed_escape() {
+  echo "$@" | sed -e 's/[]\/$*.^;|{}()[]/\\&/g' || return "$?"
+  return 0
+}
 export_function_for_sh sed_escape
 
 # Prints current shell name
@@ -47,7 +42,7 @@ get_process_depth() {
 }
 export_function_for_sh get_process_depth
 
-init() {
+update_shell_info() {
   if [ -z "${PARENTS_COUNT_ROOT_SHELL}" ]; then
     export PARENTS_COUNT_ROOT_SHELL
     PARENTS_COUNT_ROOT_SHELL="$(get_process_depth)"
@@ -56,24 +51,6 @@ init() {
   PARENTS_COUNT="$((PARENTS_COUNT - PARENTS_COUNT_ROOT_SHELL))"
 
   CURRENT_SHELL_NAME="$(get_current_shell)" || return "$?"
-
-  # ----------------------------------------
-  # Settings
-  # ----------------------------------------
-  export EDITOR=vim
-
-  # We need "\[" and "\]" to help bash understand, that it is colors (otherwise bash will break when we navigate in commands history).
-  # But we add them in get_function_definition_with_colors_replaced.
-  _C_TEXT="\033[38;5;02m"
-  _C_ERROR="\033[38;5;01m"
-  _C_SUCCESS="\033[38;5;02m"
-  _C_BORDER_USUAL="\033[38;5;27m"
-  _C_BORDER_ROOT="\033[38;5;90m"
-  _C_RESET='\e[0m'
-
-  # From 1 to 9 - The number of decimals for the command execution time
-  export accuracy=2
-  # ----------------------------------------
 
   # ----------------------------------------
   # Calculations
@@ -88,24 +65,16 @@ init() {
   if [ "${is_root}" = "1" ]; then
     # Different color for root
     export _C_BORDER="${_C_BORDER_ROOT}"
+
     export sudo_prefix=""
   else
     export _C_BORDER="${_C_BORDER_USUAL}"
+
     export sudo_prefix="sudo "
   fi
-
-  # We need to calculate this via loop, because in sh operator "**" does not exist
-  export accuracy_tens=1
-  i=0
-  while [ "${i}" -lt "${accuracy}" ]; do
-    accuracy_tens="$((accuracy_tens * 10))"
-    i="$((i + 1))"
-  done
   # ----------------------------------------
 
   if [ "${CURRENT_SHELL_NAME}" = "bash" ]; then
-    echo INIT BASH
-
     # Braces "\[" and "\]" are required, so "bash" can understand, that this is colors and not output.
     # If we do not use them, the shell will break when we try to navigate in commands' history.
     C_TEXT="\[${_C_TEXT}\]"
@@ -115,28 +84,7 @@ init() {
     C_BORDER_ROOT="\[${_C_BORDER_ROOT}\]"
     C_BORDER="\[${_C_BORDER}\]"
     C_RESET="\[${_C_RESET}\]"
-
-    # ----------------------------------------
-    # From Debian .bashrc
-    # ----------------------------------------
-    # don't put duplicate lines or lines starting with space in the history.
-    # See bash(1) for more options
-    HISTCONTROL=ignoreboth
-
-    # append to the history file, don't overwrite it
-    shopt -s histappend
-
-    # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-    HISTSIZE=1000
-    HISTFILESIZE=2000
-
-    # check the window size after each command and, if necessary,
-    # update the values of LINES and COLUMNS.
-    shopt -s checkwinsize
-  # ----------------------------------------
   else
-    echo INIT SH
-
     # "sh" does not have commands' history, and braces will result in just text, so we don't use them here
     C_TEXT="${_C_TEXT}"
     C_ERROR="${_C_ERROR}"
@@ -148,7 +96,6 @@ init() {
   fi
   return 0
 }
-init
 export_function_for_sh init
 
 # For some reason, "echo" in "sh" does not recognize "-e" option, so we do not use it
@@ -161,37 +108,15 @@ my_echo_en() {
 }
 export_function_for_sh my_echo_en
 
-ps1_function_for_TEMPLATE() {
-  init || return "$?"
-  my_echo_en "${C_BORDER}${PARENTS_COUNT} ${CURRENT_SHELL_NAME} \$ ${C_RESET}"
-  return 0
-}
-export_function_for_sh ps1_function_for_TEMPLATE ps1_function_for_bash
-export_function_for_sh ps1_function_for_TEMPLATE ps1_function_for_sh
-
 ps1_function() {
-  if [ "${CURRENT_SHELL_NAME}" = "bash" ]; then
-    ps1_function_for_bash
-  else
-    ps1_function_for_sh
-  fi
+  update_shell_info || return "$?"
+  my_echo_en "${C_BORDER}${PARENTS_COUNT} ${CURRENT_SHELL_NAME} \$ ${C_RESET}"
   return 0
 }
 export_function_for_sh ps1_function
 
-ps2_function_for_TEMPLATE() {
-  my_echo_en "${C_BORDER}${PARENTS_COUNT} ${CURRENT_SHELL_NAME} > ${C_RESET}"
-  return 0
-}
-export_function_for_sh ps2_function_for_TEMPLATE ps2_function_for_bash
-export_function_for_sh ps2_function_for_TEMPLATE ps2_function_for_sh
-
 ps2_function() {
-  if [ "${CURRENT_SHELL_NAME}" = "bash" ]; then
-    ps2_function_for_bash
-  else
-    ps2_function_for_sh
-  fi
+  my_echo_en "${C_BORDER}${PARENTS_COUNT} ${CURRENT_SHELL_NAME} > ${C_RESET}"
   return 0
 }
 export_function_for_sh ps2_function
@@ -204,3 +129,56 @@ export PS1="\$(
 export PS2='$(
   ps2_function
 )'
+
+# ----------------------------------------
+# Settings
+# ----------------------------------------
+export EDITOR=vim
+
+# We need "\[" and "\]" to help bash understand, that it is colors (otherwise bash will break when we navigate in commands history).
+# But we add them in get_function_definition_with_colors_replaced.
+export _C_TEXT="\033[38;5;02m"
+export _C_ERROR="\033[38;5;01m"
+export _C_SUCCESS="\033[38;5;02m"
+export _C_BORDER_USUAL="\033[38;5;27m"
+export _C_BORDER_ROOT="\033[38;5;90m"
+export _C_RESET='\e[0m'
+
+# From 1 to 9 - The number of decimals for the command execution time
+export accuracy=2
+# ----------------------------------------
+
+if [ "${CURRENT_SHELL_NAME}" = "bash" ]; then
+  # ----------------------------------------
+  # From Debian .bashrc
+  # ----------------------------------------
+  # don't put duplicate lines or lines starting with space in the history.
+  # See bash(1) for more options
+  HISTCONTROL=ignoreboth
+
+  # append to the history file, don't overwrite it
+  shopt -s histappend
+
+  # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+  HISTSIZE=1000
+  HISTFILESIZE=2000
+
+  # check the window size after each command and, if necessary,
+  # update the values of LINES and COLUMNS.
+  shopt -s checkwinsize
+# ----------------------------------------
+fi
+
+# ----------------------------------------
+# Calculations
+# ----------------------------------------
+# We need to calculate this via loop, because in sh operator "**" does not exist
+export accuracy_tens=1
+i=0
+while [ "${i}" -lt "${accuracy}" ]; do
+  accuracy_tens="$((accuracy_tens * 10))"
+  i="$((i + 1))"
+done
+# ----------------------------------------
+
+update_shell_info
