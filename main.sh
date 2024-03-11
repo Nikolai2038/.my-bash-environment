fe66c874bee75493f930f169d4627459b1ee173d
Nikolai2038 <nikolai2038@yandex.ru>
2024-03-11 22:46:47 +0300

    Add aliases

--- main.sh
+++ main.sh
@@ -60,7 +60,7 @@ if pstree --version 2> /dev/null; then
   export IS_PSTREE=1
 else
   export IS_PSTREE=0
-  echo "Command \"pstree\" not found! It is needed to show shell tree in \"PS1\". Try \"sudo apt-get install -y psmisc\" to install it." >&2
+  echo 'Command "pstree" not found! It is needed to show shell tree in "PS1". Try "sudo apt-get install -y psmisc" to install it.' >&2
 fi
 
 # Creates variable, which contains full function declaration and body.
@@ -118,7 +118,7 @@ update_shell_info() {
   else
     export _C_BORDER="${_C_BORDER_USUAL}"
     export sudo_prefix="sudo "
-    export PS_SYMBOL="\$"
+    export PS_SYMBOL='$'
   fi
 
   if [ "${CURRENT_SHELL_NAME}" = "bash" ]; then
@@ -144,8 +144,10 @@ export_function_for_sh update_shell_info
 # For some reason, "echo" in "sh" does not recognize "-e" option, so we do not use it
 my_echo_en() {
   if [ "${CURRENT_SHELL_NAME}" = "bash" ]; then
+    # shellcheck disable=SC3037
     echo -en "$@"
   else
+    # shellcheck disable=SC3037
     echo -n "$@"
   fi
 }
@@ -191,6 +193,104 @@ export PS2='$(
 
 update_shell_info
 
+# ========================================
+# Aliases
+# ========================================
+# To use aliases in sudo too
+alias sudo="sudo "
+
+# We use some functions as aliases.
+# But we must unalias functions' names if they exist, because alias has more priority than function.
+
+# ----------------------------------------
+# ls aliases
+# ----------------------------------------
+# 1. Detailed list
+unalias lls > /dev/null 2>&1
+lls() {
+  # We use "sed" to remove "total".
+  # shellcheck disable=SC2012
+  ls -F --group-directories-first --color -l --human-readable --time-style=long-iso "${@}" || return "$?"
+  return 0
+}
+
+# 2. Detailed list with hidden files
+alias llsa="ll --almost-all"
+
+# 3. Simple list
+unalias ll > /dev/null 2>&1
+ll() {
+  # We don't use "-1" from "ls" because it does not show us where links are pointing.
+  # Instead, we use "cut".
+  # We use "tr" to remove duplicate spaces - for "cut" to work properly.
+  lls "${@}" | tr -s '[:blank:]' | cut -d ' ' -f 8- || return "$?"
+  return 0
+}
+
+# 4. Simple list with hidden files
+alias lla="ll --almost-all"
+
+# 5. Simple list without hidden files (Markdown format)
+unalias llm > /dev/null 2>&1
+llm() {
+  # shellcheck disable=2016
+  ll "${@}" | sed -E 's/^(.*)$/- `\1`;/' || return "$?"
+  return 0
+}
+
+# 6. Simple list with hidden files (Markdown format)
+alias llam="llm --almost-all"
+alias llma="llam"
+# ----------------------------------------
+
+# Use as alias but without space
+unalias examples > /dev/null 2>&1
+examples() {
+  less -R << EOF
+$(curl "https://cheat.sh/${*}")
+EOF
+  return 0
+}
+
+# APT aliases
+# (This is not working in this script, so we still use "apt-get" everywhere here)
+alias apt="apt-get"
+# shellcheck disable=2139
+alias au="${sudo_prefix}apt-get update && ${sudo_prefix}apt-get dist-upgrade -y && ${sudo_prefix}apt-get autoremove -y"
+unalias ai > /dev/null 2>&1
+ai() {
+  # shellcheck disable=2086
+  ${sudo_prefix}apt-get update || return "$?"
+  # shellcheck disable=2086
+  ${sudo_prefix}apt-get install -y "$@" || return "$?"
+  # shellcheck disable=2086
+  ${sudo_prefix}apt-get autoremove -y || return "$?"
+  return 0
+}
+unalias ar > /dev/null 2>&1
+ar() {
+  # shellcheck disable=2086
+  ${sudo_prefix}apt-get remove -y "$@" || return "$?"
+  # shellcheck disable=2086
+  ${sudo_prefix}apt-get autoremove -y || return "$?"
+  return 0
+}
+
+# GIT aliases
+alias gs="git status"
+alias gl="git log --pretty=oneline"
+alias ga="git add ."
+alias gc="git commit -m"
+alias gac="ga && gc"
+alias gp="git push"
+unalias gacp > /dev/null 2>&1
+gacp() {
+  gac "${@}" || return "$?"
+  gp || return "$?"
+  return 0
+}
+# ========================================
+
 export my_prefix=""
 
 echo_if_messages() {
@@ -204,7 +304,6 @@ echo_if_messages "${my_prefix}Nikolai's .my-bash-environment v.0.3.0" >&2
 # ========================================
 # Autoupdate
 # ========================================
-# shellcheck disable=3044
 if [ "${CURRENT_SHELL_NAME}" = "bash" ]; then
   # We save errors status, so we can print them after "clear"
   was_autoupdate_failed=0
@@ -215,6 +314,7 @@ if [ "${CURRENT_SHELL_NAME}" = "bash" ]; then
   postfix=" # n2038 .my-bash-environment"
   postfix_escaped="$(sed_escape "${postfix}")"
 
+  # shellcheck disable=3028,3054
   this_script_path="$(realpath "${BASH_SOURCE[0]}")"
 
   using_script_path="$(sed -En "s/^source[[:blank:]]+\"?([^[:blank:]\"]+?)\"?[[:blank:]]*?${postfix_escaped}\$/\\1/p" "${bashrc_file}")"
@@ -223,7 +323,7 @@ if [ "${CURRENT_SHELL_NAME}" = "bash" ]; then
   if [ -z "${using_script_path}" ]; then
     # This script will be the one to be used.
     # We also replace user's home path with variable to make it more mobile.:wq
-    # shellcheck disable=2016
+    # shellcheck disable=2016,3060
     using_script_path="${this_script_path//"${HOME}"/'${HOME}'}"
 
     # Install it
