e3e5753d37064246aab6080da927e4c81712b7bc
Nikolai2038 <nikolai2038@yandex.ru>
2024-05-03 03:14:43 +0300

    Add more docker-compose aliases

--- main.sh
+++ main.sh
@@ -388,6 +388,18 @@ alias dcbu='dcb && dcu'
 alias dcbd='dcb && dcd'
 alias dcdu='dcd && dcu'
 alias dcbdu='dcb && dcd && dcu'
+alias dcl='docker-compose logs'
+unalias dce > /dev/null 2>&1
+dce() {
+  docker-compose exec -it "$@" bash
+  return 0
+}
+unalias dcec > /dev/null 2>&1
+dcec() {
+  container_name="${1}" && { shift || true; }
+  docker-compose exec -it "${container_name}" bash -c "$*"
+  return 0
+}
 unalias di > /dev/null 2>&1
 di() {
   info=$(docker image list --format "${_C_SUCCESS}{{.Repository}}${_C_TEXT}:${_C_BORDER_USUAL}{{.Tag}}${_C_TEXT} ({{.Size}})${_C_RESET}" --filter "dangling=false" | grep -v '<none>' | sort)
