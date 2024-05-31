4d7d76c6b732b0f65a5fc4c2e1933a8319e1646b
Nikolai2038 <nikolai2038@yandex.ru>
2024-05-31 10:43:34 +0300

    Update Pacman aliases

--- includes/aliases/pacman.sh
+++ includes/aliases/pacman.sh
@@ -4,15 +4,16 @@
 # Pacman aliases
 # ----------------------------------------
 # Update packages
+# Passing two "--refresh" flags will force a refresh of all package databases, even if they appear to be up-to-date.
 # shellcheck disable=2139
-alias pu="${sudo_prefix}pacman -Syy"
+alias pu="${sudo_prefix}pacman --noconfirm --sync --refresh --refresh --needed --sysupgrade"
 
 # Install packages
 # shellcheck disable=2139
-alias pi="${sudo_prefix}pacman -Sy --noconfirm --needed"
+alias pi="${sudo_prefix}pacman --noconfirm --sync --refresh --needed"
 
 # Remove packages
 # "pr" command exists (convert text files for printing), but I don't use it.
 # shellcheck disable=2139
-alias pr="${sudo_prefix}pacman -Runs --noconfirm"
+alias pr="${sudo_prefix}pacman --noconfirm --remove --unneeded --nosave --recursive"
 # ----------------------------------------
