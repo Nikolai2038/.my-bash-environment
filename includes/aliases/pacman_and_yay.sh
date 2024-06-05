4f4286f8fd856cc0e5592962fff6016f98405722
Nikolai2038 <nikolai2038@yandex.ru>
2024-06-06 00:58:48 +0300

    Do not run "yay" with sudo

--- includes/aliases/pacman_and_yay.sh
+++ includes/aliases/pacman_and_yay.sh
@@ -8,17 +8,17 @@
 # We rebuild the kernel to avoid errors on reboot, if some required packages were updated.
 # shellcheck disable=2139
 alias pu="${sudo_prefix}pacman --noconfirm --sync --refresh --refresh --needed --sysupgrade && ${sudo_prefix}mkinitcpio -P"
-alias yu="${sudo_prefix}yay --noconfirm --sync --refresh --refresh --needed --sysupgrade && ${sudo_prefix}mkinitcpio -P"
-alias puyu="${sudo_prefix}pacman --noconfirm --sync --refresh --refresh --needed --sysupgrade && ${sudo_prefix}yay --noconfirm --sync --refresh --refresh --needed --sysupgrade && ${sudo_prefix}mkinitcpio -P"
+alias yu="yay --noconfirm --sync --refresh --refresh --needed --sysupgrade && ${sudo_prefix}mkinitcpio -P"
+alias puyu="${sudo_prefix}pacman --noconfirm --sync --refresh --refresh --needed --sysupgrade && yay --noconfirm --sync --refresh --refresh --needed --sysupgrade && ${sudo_prefix}mkinitcpio -P"
 
 # Install packages
 # shellcheck disable=2139
 alias pi="${sudo_prefix}pacman --noconfirm --sync --refresh --needed"
-alias yi="${sudo_prefix}yay --noconfirm --sync --refresh --needed"
+alias yi="yay --noconfirm --sync --refresh --needed"
 
 # Remove packages
 # "pr" command exists (convert text files for printing), but I don't use it.
 # shellcheck disable=2139
 alias pr="${sudo_prefix}pacman --noconfirm --remove --unneeded --nosave --recursive"
-alias yr="${sudo_prefix}yay --noconfirm --remove --unneeded --nosave --recursive"
+alias yr="yay --noconfirm --remove --unneeded --nosave --recursive"
 # ----------------------------------------
